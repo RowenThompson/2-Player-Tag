0d95ea0afbd64025b89f3f054ffcd3f6fb82ff7f
RowenThompson <73656188+RowenThompson@users.noreply.github.com>
2024-01-17 18:30:18 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,66 +1,71 @@
 import pygame, sys
-#screen
-class Displaysurf():
-    def __init__(self, width=1200, height=800):
-        self.width = width
-        self.height = height
-        self.resolution = (self.width, self.height)
-        self.displaysurf = pygame.display.set_mode(self.resolution)
-    def show(self):
-        pygame.display.set_mode(self.resolution)
-spawnpoint_1_x = Displaysurf().width/25
-spawnpoint_1_y = Displaysurf().height/25
-player_y = 0
-player_x = 0
-class Player1():
-    def __init__(self):
-        self.move_x = 0
-        self.move_y = 0
-        self.x = spawnpoint_1_x + self.move_x
-        self.y = spawnpoint_1_y + self.move_y
-        self.name = "Bob"
-        self.color = (0,0,255)
-        self.radius = 25
-        self.speed = 25
-    def show(self):
-        pygame.draw.circle(Displaysurf().displaysurf, self.color, (self.x, self.y), self.radius)
-    def controls(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                quit_game()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    quit_game()
-        keys = pygame.key.get_pressed()
-        if keys == pygame.K_w:
-            self.move_y -= self.speed
-        if keys == pygame.K_s:
-            self.move_y += self.speed
-        if keys == pygame.K_a:
-            self.move_x -= self.speed
-        if keys == pygame.K_d:
-            self.move_x += self.speed
+
+resolution = dis_width, dis_height = (500, 500)
+displaysurf = pygame.display.set_mode(resolution)
+game_name = "2 Player Tag"
+display_name = pygame.display.set_caption(game_name)
+red = (255, 0, 0)
+blue = (0, 0, 255)
+
 def quit_game():
     pygame.quit()
     sys.exit()
 
-def main():
-    pygame.init()
-    Displaysurf().show()
-    spawnpoint_1_x = Displaysurf().width/25
-    spawnpoint_1_y = Displaysurf().height/25
-    spawnpoint_2_x = Displaysurf().width/12
-    spawnpoint_2_y = Displaysurf().height/12
-    FPS = 60
-    clock = pygame.time.Clock()
-    game_state = "tag"
-    while game_state == "tag":
-        Player1().x += 5
-        Player1().controls()
-        Player1().show()
-        pygame.display.update()
-        pygame.display.flip()
-        clock.tick(FPS)
+def change_player_color(player_color, playerr_color):
+    if player_color == blue:
+        return player_color==red, playerr_color==blue
+    if playerr_color == blue:
+        return player_color==blue, playerr_color==red
 
-main()
+def game_loop():
+    pygame.init()
+    game_state = "tag"
+    player_x = 0
+    player_y = 0
+    playerr_x = 255
+    playerr_y = 255
+    speed = 0.025
+    player_color = blue
+    playerr_color = red
+    player_rect = pygame.draw.rect(displaysurf, player_color, pygame.Rect(player_x, player_y, 50, 50))
+    playerr_rect = pygame.draw.rect(displaysurf, playerr_color, pygame.Rect(playerr_x, playerr_y, 50, 50))
+
+    while game_state == "tag":
+        displaysurf.fill((0, 0, 0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quit_game()
+            if event.type == pygame.KEYDOWN:
+                pass
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            player_x -= speed
+
+        if keys[pygame.K_d]:
+            player_x += speed
+
+        if keys[pygame.K_w]:
+            player_y -= speed
+
+        if keys[pygame.K_s]:
+            player_y += speed
+
+        if keys[pygame.K_LEFT]:
+            playerr_x -= speed
+
+        if keys[pygame.K_RIGHT]:
+            playerr_x += speed
+
+        if keys[pygame.K_UP]:
+            playerr_y -= speed
+
+        if keys[pygame.K_DOWN]:
+            playerr_y += speed
+
+        if player_rect.colliderect(playerr_rect):
+            change_player_color(player_color, playerr_color)
+        player_rect = pygame.draw.rect(displaysurf, player_color, pygame.Rect(player_x, player_y, 50, 50))
+        playerr_rect = pygame.draw.rect(displaysurf, playerr_color, pygame.Rect(playerr_x, playerr_y, 50, 50))
+        pygame.display.update()
+game_loop()
 quit_game()
